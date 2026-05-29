72a2d9e78062c43b5d0748ce323c1c10f724a6d8
marclenotharwaat <marclenotharwat@gmail.com>
2026-05-29 18:52:23 +0300

    fix bug on  order

--- middleware/order/checkOrderAccess.js
+++ middleware/order/checkOrderAccess.js
@@ -1,18 +1,17 @@
 import Order from "../../models/Order.js";
 import catchAsync from "../../utils/catchAsync.js";
 import { ApiError } from "../../utils/apiError.js";
-
 export const checkOrderAccess = catchAsync(async (req, res, next) => {
-  const order = await Order.findById(req.params.id).populate(
-    "items.product user",
-  );
+  const order = await Order.findById(req.params.id).populate("items.product");
 
   if (!order) {
     return next(new ApiError("Order not found", 404));
   }
 
+  const userId = order.user;
+
   if (
-    order.user.toString() !== req.user._id.toString() &&
+    userId.toString() !== req.user._id.toString() &&
     req.user.role !== "admin"
   ) {
     return next(new ApiError("Not authorized", 403));
